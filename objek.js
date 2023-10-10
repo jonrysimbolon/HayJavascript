90443e855e44e484ee02d98b1e54a89ab4b729d8
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-10 14:53:04 +0700

    adding objek

---
+++ objek.js
@@ -0,0 +1,17 @@
+const user = {
+    firstName:"Jonry",
+    lastName:"Simbolon",
+    age:"26",
+    isMarried:false,
+    "bahasa pemrograman":"Javascript",
+}
+
+console.log(user.firstName)
+console.log(user["bahasa pemrograman"])
+
+user["bahasa pemrograman"] = 'Node'
+user["programmer kah ?"] = true
+
+delete user.age
+
+console.log(user)
