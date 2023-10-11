1b150f176bba893d8b186ac340f52e9912239d51
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-11 15:15:13 +0700

    overriding

---
+++ Oop/overriding.js
@@ -0,0 +1,62 @@
+class MailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+}
+
+class WhatsAppService extends MailService {
+  constructor(sender, isBusiness) {
+    //super(sender); // ! wajib jika melakukan pewarisan
+    this.sender = sender // !! hapus ini untuk menghilangkan error
+    this.isBusiness = isBusiness;
+  }
+}
+
+const whatsapp = new WhatsAppService("+6281234567890", true);
+
+/**
+ * Output:
+ * ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+ */
+
+
+class MailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+ 
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+
+}
+
+class WhatsAppService extends MailService {
+  constructor(sender, isBusiness) {
+    super(sender);
+    this.isBusiness = isBusiness;
+  }
+  
+ 
+  // Overriding method
+  sendMessage(message, receiver) {
+    // memanggil method sendMessage pada superclass
+    super.sendMessage(message, receiver);
+
+    console.log('message sent via WhatsApp');
+  }
+}
+
+
+const mailService = new MailService('someSender');
+const whatsappService = new WhatsAppService('+6281234567890', true);
+
+mailService.sendMessage('Hai, apa kabar?', 'someReceiver');
+whatsappService.sendMessage('Hai, apa kabar?', '+6289876543210');
+
+/**
+* Output:
+* someSender sent Hai, apa kabar? to someReceiver
+* +6281234567890 sent Hai, apa kabar? to +6289876543210
+* message sent via WhatsApp
+*/
