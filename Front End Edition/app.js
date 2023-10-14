202396083f71b2a9afd94d2e96967b7d176a9bde
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-14 23:49:13 +0700

    custom event

--- Front End Edition/app.js
+++ Front End Edition/app.js
@@ -1,22 +1,12 @@
-function welcome() {
-  alert("Sim salabim muncullah elemen-elemen HTML!");
-  const contents = document.querySelector(".contents");
-  contents.removeAttribute("hidden");
+const changeCaption = new Event('changeCaption')
+window.addEventListener('load', function(){
+    const tombol = document.getElementById('tombol')
+    tombol.addEventListener('changeCaption', customEventHandler)
+    tombol.addEventListener('click', function(){})
+})
+
+function customEventHandler(ev){
+    console.log('Event ' + ev.type + ' telah dijalankan')
+    const caption = document.getElementById('caption')
+    caption.innerText = 'Anda telah membangkitkan custom event'
 }
-
-function increment() {
-  document.getElementById("count").innerText++;
-
-  if (document.getElementById("count").innerText == 7) {
-    const hiddenMessage = document.createElement("h4");
-    hiddenMessage.innerText = "Selamat! Anda menemukan hadiah tersembunyi...\n";
-    const image = document.createElement("img");
-    image.setAttribute("src", "https://i.ibb.co/0V49VRZ/catto.jpg");
-    const contents = document.querySelector(".contents");
-    contents.appendChild(hiddenMessage).appendChild(image);
-  }
-}
-
-// kita akan menulis kode menggunakan addEventListener disini
-window.addEventListener("load", welcome);
-document.getElementById("incrementButton").addEventListener("click", increment);
