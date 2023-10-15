7bce438691ad493f4d47e8131d69297ad024d683
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-15 18:28:55 +0700

    bagian dari membangkitkan custom event

--- Front End Edition/app.js
+++ Front End Edition/app.js
@@ -2,7 +2,9 @@ const changeCaption = new Event('changeCaption')
 window.addEventListener('load', function(){
     const tombol = document.getElementById('tombol')
     tombol.addEventListener('changeCaption', customEventHandler)
-    tombol.addEventListener('click', function(){})
+    tombol.addEventListener('click', function(){
+        tombol.dispatchEvent(changeCaption)
+    })
 })
 
 function customEventHandler(ev){
