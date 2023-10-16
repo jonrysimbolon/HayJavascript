db2e96d1c531a9fb3d8621f85dca39d0276e3b97
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-16 10:26:52 +0700

    form event

--- Front End Edition/app.js
+++ Front End Edition/app.js
@@ -1,11 +1,10 @@
-const divs = document.getElementsByTagName("div");
+const submitAction = document.getElementById("formDataDiri");
 
-for (let el of divs) {
-  el.addEventListener(
-    "click",
-    function () {
-      alert("ELEMENT " + el.getAttribute("id").toUpperCase());
-    },
-    true
-  );
-}
+submitAction.addEventListener("submit", function (event) {
+  const inputNama = document.getElementById("inputNama").value;
+  const inputDomisili = document.getElementById("inputDomisili").value;
+  const hiddenMessage = `Halo, ${inputNama}. Bagaimana cuacanya di ${inputDomisili}?`;
+
+  document.getElementById("messageAfterSubmit").innerText = hiddenMessage;
+  event.preventDefault();
+});
