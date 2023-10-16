412252bb8cb2a7936d79095084cf48471c346bbd
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-16 13:00:16 +0700

    show the todo list

--- Front End Edition/todoapps/js/app.js
+++ Front End Edition/todoapps/js/app.js
@@ -1,44 +1,113 @@
-const todos = []
-const RENDER_EVENT = 'render-todo'
+const todos = [];
+const RENDER_EVENT = "render-todo";
 
-document.addEventListener('DOMContentLoaded', function(){
-    const submitForm = document.getElementById('form')
-    submitForm.addEventListener('submit', function(event){
-        event.preventDefault()
-        addTodo()
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("form");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTodo();
+  });
+});
 
-document.addEventListener(RENDER_EVENT, function(){
-    console.log(todos)
-})
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedTODOList = document.getElementById("todos");
+  uncompletedTODOList.innerHTML = "";
 
-function addTodo(){
-    const textTodo = document.getElementById('title').value
-    const timeStamp = document.getElementById('date').value
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    if (!todoItem.isCompleted) {
+      uncompletedTODOList.append(todoElement);
+    }
+  }
+});
 
-    const generateID = generateId()
-    const todoObject = generateTodoObject(generateID, textTodo, timeStamp, false)
+function addTodo() {
+  const textTodo = document.getElementById("title").value;
+  const timeStamp = document.getElementById("date").value;
 
+  const generateID = generateId();
+  const todoObject = generateTodoObject(generateID, textTodo, timeStamp, false);
 
-    todos.push(todoObject)
-    document.dispatchEvent(new Event(RENDER_EVENT))
+  todos.push(todoObject);
+  document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
-function generateId(){
-    return +new Date()
+function generateId() {
+  return +new Date();
 }
 
-function generateTodoObject(
+function generateTodoObject(id, task, timeStamp, isCompleted) {
+  return {
     id,
     task,
     timeStamp,
-    isCompleted
-){
-    return {
-        id,
-        task,
-        timeStamp,
-        isCompleted
+    isCompleted,
+  };
+}
+
+function makeTodo(todoObject) {
+  const textTitle = document.createElement('h2');
+  textTitle.innerText = todoObject.task;
+ 
+  const textTimestamp = document.createElement('p');
+  textTimestamp.innerText = todoObject.timeStamp;
+ 
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('inner');
+  textContainer.append(textTitle, textTimestamp);
+ 
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow');
+  container.append(textContainer);
+  container.setAttribute('id', `todo-${todoObject.id}`);
+
+  console.log(todoObject);
+  console.log(textContainer)
+
+  if (todoObject.isCompleted) {
+    const undoButton = document.createElement("button");
+    undoButton.classList.add("undo-button");
+
+    undoButton.addEventListener("click", function () {
+      undoTaskFromCompleted(todoObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompleted(todoObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(todoObject.id);
+    });
+
+    container.append(checkButton);
+  }
+
+  return container;
+}
+
+function addTaskToCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodo(todoId) {
+  for (const todoItem of todos) {
+    if (todoItem.id === todoId) {
+      return todoItem;
     }
+  }
+  return null;
 }
