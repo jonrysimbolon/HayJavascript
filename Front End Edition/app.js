fefc18eba32f5289ec0cb7f7ecbfde113f57e439
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-10-16 10:13:50 +0700

    Event capturing

--- Front End Edition/app.js
+++ Front End Edition/app.js
@@ -1,14 +1,11 @@
-const changeCaption = new Event('changeCaption')
-window.addEventListener('load', function(){
-    const tombol = document.getElementById('tombol')
-    tombol.addEventListener('changeCaption', customEventHandler)
-    tombol.addEventListener('click', function(){
-        tombol.dispatchEvent(changeCaption)
-    })
-})
+const divs = document.getElementsByTagName("div");
 
-function customEventHandler(ev){
-    console.log('Event ' + ev.type + ' telah dijalankan')
-    const caption = document.getElementById('caption')
-    caption.innerText = 'Anda telah membangkitkan custom event'
+for (let el of divs) {
+  el.addEventListener(
+    "click",
+    function () {
+      alert("ELEMENT " + el.getAttribute("id").toUpperCase());
+    },
+    true
+  );
 }
